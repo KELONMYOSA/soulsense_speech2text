6466924a48812eb50bd08b56645e56c429e3e351
Vladislav Samoilenko <79825223592@yandex.ru>
2023-06-05 19:25:42 +0300

    added cuda support

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from fastapi import FastAPI, UploadFile
+import os
 
+from dotenv import load_dotenv, find_dotenv
+from fastapi import FastAPI, UploadFile
 from speech_recognition import recognize
+
+load_dotenv(find_dotenv())
+WHISPER_DEVICE = os.environ.get('WHISPER_DEVICE')
 
 app = FastAPI()
 
@@ -8,6 +13,11 @@ app = FastAPI()
 @app.post("/speech2text")
 async def speech2text(file: UploadFile):
     segments = recognize(file)
-    text = list(map(lambda x: x.text, segments))
+    if WHISPER_DEVICE == "cpu":
+        text = list(map(lambda x: x.text, segments))
+    elif WHISPER_DEVICE == "cuda":
+        text = list(map(lambda x: x["text"], segments))
+    else:
+        exit(1)
 
     return text
--- speech_recognition.py
+++ speech_recognition.py
@@ -1,6 +1,7 @@
 import os
-
+import whisper
 import numpy as np
+
 from dotenv import load_dotenv, find_dotenv
 from pydub import AudioSegment
 from pywhispercpp.model import Model
@@ -8,13 +9,23 @@ from pywhispercpp.model import Model
 load_dotenv(find_dotenv())
 WHISPER_MODEL = os.environ.get('WHISPER_MODEL')
 WHISPER_THREADS = os.environ.get('WHISPER_THREADS')
+WHISPER_LANGUAGE = os.environ.get('WHISPER_LANGUAGE')
+WHISPER_DEVICE = os.environ.get('WHISPER_DEVICE')
 
 
 def recognize(file):
-    audio_array = audio_to_array(file.file)
-
-    model = Model(WHISPER_MODEL, n_threads=int(WHISPER_THREADS), language="ru")
-    segments = model.transcribe(audio_array)
+    if WHISPER_DEVICE == "cpu":
+        audio_array = audio_to_array(file.file)
+        model = Model(WHISPER_MODEL, n_threads=int(WHISPER_THREADS), language=WHISPER_LANGUAGE)
+        segments = model.transcribe(audio_array)
+    elif WHISPER_DEVICE == "cuda":
+        audio_file = save_temp_audio(file)
+        model = whisper.load_model(WHISPER_MODEL, WHISPER_DEVICE)
+        segments = model.transcribe(audio_file, language=WHISPER_LANGUAGE)["segments"]
+        os.remove(audio_file)
+    else:
+        print("Incorrect WHISPER_DEVICE")
+        exit(1)
 
     return segments
 
@@ -26,4 +37,13 @@ def audio_to_array(file):
     samples = [s.get_array_of_samples() for s in channel_sounds]
     arr = np.array(samples).T.astype(np.float32)
     arr /= np.iinfo(samples[0].typecode).max
+
     return arr
+
+
+def save_temp_audio(file):
+    file_location = f"temp_audio/{file.filename}"
+    with open(file_location, "wb+") as file_object:
+        file_object.write(file.file.read())
+
+    return file_location
